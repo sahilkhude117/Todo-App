905455a7762e983d77220667c7a33091f1854e8b
Sahil Khude <sahilkhude11@gmail.com>
2024-11-21 14:16:03 +0530

    Added useEffect to fetch call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,14 @@ import { Todos } from './components/Todos'
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos")
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
    .then(async function (res) {
      const json = await res.json();
      setTodos(json.todos);
    })
-
+  },[])
+  
   return (
     <div>
       <CreateTodo></CreateTodo>
